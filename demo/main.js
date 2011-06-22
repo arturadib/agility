6025ee736cbb56be4c6d5a0b53325b32e3ac773d
Artur Adib <arturadib@gmail.com>
2011-06-22 18:00:57 -0400

    Working 'add item' example!

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,15 @@
-// One-liners: one item
-var hello = $$('Hello World', '<button>${content}</button>', {
-  'click root': function(event){
-    alert(event);
+// // One-liners: one item
+// var hello = $$('Hello World', '<button>${content}</button>', {
+//   'click root': function(event){
+//     alert(event);
+//   }
+// });
+// $$.document.add(hello);
+
+var list = $$({}, '<div><button>Add item</button> <ul id="list"></ul></div>', {
+  'click button': function(){
+    var item = $$('Hello World', '<li>${content}</li>'); // prototype
+    this.add(item, '#list'); // now newItem.parent == this; calls view:add($$obj, '#list'), which will append to specified element
   }
 });
-$$.document.add(hello);
+$$.document.add(list);
