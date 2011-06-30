795c03427ce7e8c4d86d561cd0730fc3869f0dab
Artur Adib <arturadib@gmail.com>
2011-06-30 09:02:43 -0400

    Picshow uses data-bind

--- demo/picshow/main.js
+++ demo/picshow/main.js
@@ -1,56 +1,47 @@
 
-
-// !!!!!
-
-
-// CREATE CHECKBOXES FOR EACH PICTURE TO DO BATCH-ERASE PICS
-
-
 var pic = $$({ 
   // Custom
   checked: false,
   
-  // Agility view
   view: {
     format: '<div>\
-                  <div id="opts"> <input type="checkbox" id="check"/> <button id="destroy">X</button> </div>\
-                  <div id="img-container"> <img src="${src}"/> </div> \
-               </div>',
+                <div id="opts"> <input type="checkbox" id="check"/> <button id="remove">X</button> </div>\
+                <div id="img-container"> <img data-bind="path src"/> </div> \
+             </div>',
     style: '& { float:left; margin-right:40px; margin-bottom:20px; width:100px; } \
             & div#opts { } \
             & input#check { float:left; } \
-            & button#destroy { float:right; } \
+            & button#remove { float:right; } \
             & div#img-container { clear:both; border:1px solid black; width:100px; height:100px; text-align:center; } \
             & img { max-width:100px; max-height:100px; }'
   },
   
-  // Agility controller
   controller: {
     'click img': function(){
       this.view.$('input#check').click();
     },
-    'click button#destroy': function(){
-      this.destroy();
+    'click button#remove': function(){
+      this.remove();
     },
     'change input#check': function(event){
-      this.checked = $(event.target).is(':checked');
+      this.checked = $(event.target).prop('checked');
     }    
   }
 });
 
 var app = $$({
   view: {
-    format: '<div>\
-                 <button id="add">Add picture</button> \
-                 <div id="list"/> \
-               </div>', 
+    format: '<div> \
+               <button id="add">Add picture</button> \
+               <div id="list"/> \
+             </div>', 
     style: '& { margin-top:20px; width:600px; margin-left:auto; margin-right:auto; }  \
             & > #add { font-size:120%; } \
             & > #list { margin-top:20px; }'
   },
   controller: {
     'click button#add': function(){
-      var newPic = $$(pic, {src:"http://www.publicdomainpictures.net/pictures/5000/nahled/1-1252593298WHAn.jpg"});
+      var newPic = $$(pic, {path:"http://www.publicdomainpictures.net/pictures/5000/nahled/1-1252593298WHAn.jpg"});
       this.add(newPic, '#list');
     }
   }
