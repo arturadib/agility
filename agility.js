6223183bfe44c997884d511a0a8a998671e219af
Adrian Cochrane <alcinnz@eml.cc>
2011-07-30 12:00:19 +1200

    Passing rerender hook this.

--- agility.js
+++ agility.js
@@ -573,7 +573,7 @@
 
       // Triggered after model is changed
       '_change': function(event){
-        template_rerender();
+        template_rerender(this);
       }
       
     }, // controller prototype
